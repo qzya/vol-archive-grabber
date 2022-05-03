edab07330078853c4d458207b468151795ac55d0
qzya <qqqzya@gmail.com>
2022-05-03 17:51:20 +0300

    comments added

--- archive-grabber.py
+++ archive-grabber.py
@@ -8,10 +8,12 @@ headers = {}
 
 arch_id = '410045'
 
+# request the main page of the document:
 main_response = requests.get(base_url + arch_id, headers=headers)
 
+# set cookies, parse and save description, make directories
 if (main_response.status_code == 200):
-
+    # get coockies from response and set for global headers to use in get_images() function
     for cookie in main_response.cookies:
         if cookie.name == 'PHPSESSID':
             headers['Cookie'] = f'{cookie.name}={cookie.value}'
@@ -27,6 +29,7 @@ if (main_response.status_code == 200):
 else:
     print('Ошибка! Возможно неверно указан номер дела')
 
+# Document`s first page number handling
 while True:
     pages_range = [input('Начальная страница:\n')]
     if (not pages_range[0].isdigit()):
@@ -40,6 +43,7 @@ while True:
         else:
             break
 
+# Document`s last page number handling
 while True:
     pages_range.append(input('Конечная страница:\n'))
     if (not pages_range[1].isdigit() or int(pages_range[0]) >= int(pages_range[1])):
@@ -54,13 +58,12 @@ while True:
         else:
             break
 
-
-
-
+# requests and saves images, checks status code and empty bytes object
 def get_images():
     pages_count = 0
     for i in range(pages_range[0], pages_range[1]+1):
         image_response = requests.get(images_url + arch_id + '?n=' + str(i), headers=headers)
+
         if (image_response.status_code == 200 and bool(image_response.content) != False):
             print(f'Сохранаяем страницу #{i}...', end='\r', flush=True)
             with open(arch_id+ '/image_' + str(i) + '.jpg', 'wb') as f:
